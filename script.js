c09dc046ccf513aa85db1bab2f566e7bb690fa48
Luiz <luiz.marques1@estudante.ufla.br>
2026-09-08 14:56:49 -0300

    Adicionar sorteio das minas

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const casas = document.querySelectorAll(".casa");
+const botaoIniciar = document.getElementById("botao-iniciar");
+const seletorQuantidadeMinas = document.getElementById("quantidade-minas");
+
+let casasComMinas = [];
 
 casas.forEach(function (casa) {
     casa.addEventListener("click", function () {
         console.log("Casa clicada:", casa.id);
     });
 });
+
+botaoIniciar.addEventListener("click", function () {
+    sortearMinas();
+});
+
+function sortearMinas() {
+    casasComMinas = [];
+
+    const quantidadeMinas = Number(seletorQuantidadeMinas.value);
+
+    while (casasComMinas.length < quantidadeMinas) {
+        const numeroCasa = Math.floor(Math.random() * 25) + 1;
+        const idCasa = `casa-${numeroCasa}`;
+
+        if (!casasComMinas.includes(idCasa)) {
+            casasComMinas.push(idCasa);
+        }
+    }
+
+    console.log("Casas com minas:", casasComMinas);
+}
