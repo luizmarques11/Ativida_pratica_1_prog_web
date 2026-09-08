1e0fdcd6203727281ff52b341127eff87436669e
Luiz <luiz.marques1@estudante.ufla.br>
2026-09-08 14:38:41 -0300

    Adionar eventos de clique nas casas

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Jogo carregado.");
+const casas = document.querySelectorAll(".casa");
+
+casas.forEach(function (casa) {
+    casa.addEventListener("click", function () {
+        console.log("Casa clicada:", casa.id);
+    });
+});
