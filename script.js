faca4d2be42245cc15069a2af8bb9ff9730d6ced
Luiz <luiz.marques1@estudante.ufla.br>
2026-09-09 09:11:19 -0300

    Adicionar condicao de vitoria e revelar minas ao perder a partida

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const elementoPontos = document.getElementById("pontos");
 let casasComMinas = [];
 let jogoAtivo = false;
 let pontos = 0;
+let quantidadeMinas = 0;
 
 casas.forEach(function (casa) {
     casa.addEventListener("click", function () {
@@ -21,6 +22,8 @@ botaoIniciar.addEventListener("click", function () {
 function iniciarJogo() {
     jogoAtivo = true;
     pontos = 0;
+
+    quantidadeMinas = Number(seletorQuantidadeMinas.value);
 
     elementoPontos.textContent = pontos;
 
@@ -39,8 +42,6 @@ function limparTabuleiro() {
 
 function sortearMinas() {
     casasComMinas = [];
-
-    const quantidadeMinas = Number(seletorQuantidadeMinas.value);
 
     while (casasComMinas.length < quantidadeMinas) {
         const numeroCasa = Math.floor(Math.random() * 25) + 1;
@@ -71,11 +72,33 @@ function verificarCasa(casa) {
         jogoAtivo = false;
 
         mensagem.textContent = "Você encontrou uma mina!";
+
+        revelarMinas();
     } else {
         casa.textContent = "💎";
 
         pontos++;
 
         elementoPontos.textContent = pontos;
+
+        verificarVitoria();
     }
+}
+
+function verificarVitoria() {
+    const quantidadeCasasSeguras = 25 - quantidadeMinas;
+
+    if (pontos === quantidadeCasasSeguras) {
+        jogoAtivo = false;
+
+        mensagem.textContent = "Você venceu!";
+    }
+}
+
+function revelarMinas() {
+    casas.forEach(function (casa) {
+        if (casasComMinas.includes(casa.id)) {
+            casa.textContent = "💣";
+        }
+    });
 }
